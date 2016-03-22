495159daa9ba11e3b1558730aa1c9aed079605a7
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-21 23:02:20 -0400

    First commit

---
+++ AppHouse-Showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  AppHouse-Showcase
+//
+//  Created by Emanuel  Guerrero on 3/21/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
---
+++ AppHouse-Showcase/MaterialTextField.swift
@@ -0,0 +1,26 @@
+//
+//  MaterialTextField.swift
+//  AppHouse-Showcase
+//
+//  Created by Emanuel  Guerrero on 3/21/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class MaterialTextField: UITextField {
+    override func awakeFromNib() {
+        layer.cornerRadius = 2.0
+        layer.borderColor = UIColor(red: SHADOW_COLOR, green: SHADOW_COLOR, blue: SHADOW_COLOR, alpha: 0.1).CGColor
+        layer.borderWidth = 1.0
+    }
+    
+    //For placeholder
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, 10, 0)
+    }
+}
