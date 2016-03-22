7862ce2da075285c9b5afb07a256fc123cca1eee
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-22 19:06:03 -0400

    Added images for the feed. Also created the table view that will hold the post.

---
+++ AppHouse-Showcase/FeedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewController.swift
+//  AppHouse-Showcase
+//
+//  Created by Emanuel  Guerrero on 3/22/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
+    }
+}
---
+++ AppHouse-Showcase/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  AppHouse-Showcase
+//
+//  Created by Emanuel  Guerrero on 3/22/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var showcaseImage: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        profileImage.layer.cornerRadius = profileImage.frame.size.width / 2
+        profileImage.clipsToBounds = true
+        
+        showcaseImage.clipsToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
