fd02b3c6f1ba2936167b94cd77571f21c8a45d0d
Emanuel  Guerrero <Emanuel@Emanuels-MacBook-Pro.local>
2016-03-24 23:07:42 -0400

    Created a model class for a post and had Firebase load all the posts.

--- AppHouse-Showcase/FeedViewController.swift
+++ AppHouse-Showcase/FeedViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +22,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         DataService.dataService.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
+            
+            self.posts = []
+            
+            if let snapshots = snapshot.children.allObjects as? [FDataSnapshot] {
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDictionary = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDictionary)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            
             self.tableView.reloadData()
         })
     }
@@ -28,10 +46,13 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
 }
---
+++ AppHouse-Showcase/Post.swift
@@ -0,0 +1,59 @@
+//
+//  Post.swift
+//  AppHouse-Showcase
+//
+//  Created by Emanuel  Guerrero on 3/24/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String!
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    var postKey: String {
+        return _postKey
+    }
+    
+    init(description: String, imageUrl: String?, username: String) {
+        _postDescription = description
+        _imageUrl = imageUrl
+        _username = username
+    }
+    
+    init(postKey: String, dictionary: Dictionary<String, AnyObject>) {
+        _postKey = postKey
+        
+        if let likes = dictionary["likes"] as? Int {
+            _likes = likes
+        }
+        
+        if let imageUrl = dictionary["imageUrl"] as? String {
+            _imageUrl = imageUrl
+        }
+        
+        if let description = dictionary["description"] as? String {
+            _postDescription = description
+        }
+    }
+}
